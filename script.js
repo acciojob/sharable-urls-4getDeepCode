458f1991e7bd5be5d25acbf6ab8b866aebe35c5a
Deep Chand <deepc2060@gmail.com>
2026-03-29 21:17:16 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,29 @@
 // your code here
+const form = document.getElementById("form");
+const nameInput = document.getElementById("name");
+const yearInput = document.getElementById("year");
+const urlText = document.getElementById("url");
+
+form.addEventListener("submit",function(e){
+	e.preventDefault();
+
+	let name = nameInput.value.trim();
+	let year = yearInput.value.trim();
+
+	let base = "https://localhost:8080/";
+    let query = [];
+
+	if(name){
+		query.push("name="+encodeURIComponent(name))
+	}
+	if(year){
+	    query.push("year=" + encodeURIComponent(year));
+    }
+
+    if(query.length > 0){
+        urlText.textContent = base + "?" + query.join("&");
+    }else{
+        urlText.textContent = base;
+    }
+	
+})
